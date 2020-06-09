8f9d589ed442abbbece1b0a2009ab7ced06d47da
rplenzdorf <seifertravi@hotmail.de>
2020-06-09 10:00:32 +0200

    test2

--- main.py
+++ main.py
@@ -6,11 +6,18 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+
+p=5
+n=10
+w=1
+
 class interface(App):
     def build(self):
         root_widget = BoxLayout(orientation="vertical")
 
-        output_label = Label(size_hint_y=1)
+        Erkennung = GridLayout(cols = 2,size_hint_y=1.5)
+        Erkennung.add_widget(Label(text='Anmomalie'))
+        Erkennung.add_widget(Button(text="AN"))
 
         button_symbols = ('1', '2', '3', '+',
                           '4', '5', '6', '-',
@@ -21,13 +28,32 @@ class interface(App):
         for symbol in button_symbols:
             button_grid.add_widget(Button(text=symbol))
 
-        clear_button = Button(text='clear',
+        input_grid = GridLayout(cols=3,size_hint_y=2)
+        for i in range(3):
+            input_grid.add_widget(Button(text="+"))
+        
+        input_grid.add_widget(Label(text="Leistung:\n"+str(p)))
+        input_grid.add_widget(Label(text="Drehzahl"))
+        input_grid.add_widget(Label(text="Wind:\n"+str(w)))
+
+        for i in range(3):
+            input_grid.add_widget(Button(text="-"))
+        
+        hack_button = Button(text='Überschreiben',
                               size_hint_y=None,
                               height=100)
 
-        root_widget.add_widget(output_label)
-        root_widget.add_widget(button_grid)
-        root_widget.add_widget(clear_button)
+        def p_up(p):
+            input_grid.children[5].text="Leistung:\n"+str(20)                 
+        input_grid.children[8].bind(on_press=(p_up))
+
+        def p_down(p):
+            input_grid.children[5].text="Leistung:\n"+str(0)                   
+        input_grid.children[2].bind(on_press=(p_down))
+
+        root_widget.add_widget(Erkennung)
+        root_widget.add_widget(input_grid)
+        root_widget.add_widget(hack_button)
 
 
         return root_widget
